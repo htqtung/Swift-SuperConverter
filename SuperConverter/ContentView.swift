9e61097deb5b0b2b360ed1c003e9ea73f8a21d2f
Tung Huynh <htqtung@gmail.com>
2023-04-27 18:14:35 +0300

    added f to c, oz to ml, ft to m, mi to km

--- SuperConverter/ContentView.swift
+++ SuperConverter/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var degreeInF: Double = 0
+    var degreeInC: Double {
+        return (degreeInF - 32) * (5 / 9)
+    }
+    
+    @State private var volumeInOz: Double = 0
+    var volumeInMl: Double {
+        return volumeInOz * 29.574
+    }
+    
+    @State private var lengthInFt: Double = 0
+    var lengthInMeter: Double {
+        return lengthInFt / 3.281
+    }
+    
+    @State private var lengthInMile: Double = 0
+    var lengthInKm: Double {
+        return lengthInMile * 1.609
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                // Fahrenheit to Celsius
+                Section {
+                    TextField("Temperature in F", value: $degreeInF, format: .number)
+                    Text("\(degreeInF, specifier: "%.2f")°F = \(degreeInC, specifier: "%.2f")°C").foregroundColor(Color.blue).padding()
+                } header: {
+                    Text("Fahrenheit to Celsius")
+                }
+                
+                // Oz to ml
+                Section {
+                    TextField("Volume in oz", value: $volumeInOz, format: .number)
+                    Text("\(volumeInOz, specifier: "%.2f")oz = \(volumeInMl, specifier: "%.2f")ml").foregroundColor(Color.blue).padding()
+                } header: {
+                    Text("Fluid ounce to Mililiter")
+                }
+                
+                // Ft to m
+                Section {
+                    TextField("Length in ft", value: $lengthInFt, format: .number)
+                    Text("\(lengthInFt, specifier: "%.2f")ft = \(lengthInMeter, specifier: "%.2f")m").foregroundColor(Color.blue).padding()
+                } header: {
+                    Text("Foot to Meter")
+                }
+                
+                // Mile to Kilometer
+                Section {
+                    TextField("Length in mile", value: $lengthInMile, format: .number)
+                    Text("\(lengthInMile, specifier: "%.2f")mi = \(lengthInKm, specifier: "%.2f")km").foregroundColor(Color.blue).padding()
+                } header: {
+                    Text("Mile to Kilometer")
+                }
+            }
+            .navigationTitle("Super Converter")
         }
-        .padding()
     }
 }
 
